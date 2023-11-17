0286bfbc54001f1551c8d185d4e7e83ff2bf9f44
Edi Supriyadi <eddydeniro@users.noreply.github.com>
2023-11-17 17:20:25 +0700

    Add files via upload

--- includes/header_dropdown_menu.php
+++ includes/header_dropdown_menu.php
@@ -2,7 +2,7 @@
 global ${ROOX_PLUGIN . "_modules"};
 foreach (${ROOX_PLUGIN . "_modules"} as $module) 
 {
-    $path = component_path(ROOX_PLUGIN . "/{$module}/bottom");
+    $path = component_path(ROOX_PLUGIN . "/{$module}/header_menu");
     if(is_file($path))
     {
         require $path;
--- includes/layout_head.php
+++ includes/layout_head.php
@@ -1,11 +1,62 @@
+<style>
+    .transparent{
+        border: none;
+        background: none;
+    }
+    .input_adj{
+        display: inline-block;
+    }
+    .text13{
+        font-size: 13px !important;
+    }
+    .roox-alert{
+        position: absolute;
+        top: 50px;
+        right: 0px;
+        min-width: 200px;
+        max-width: 500px;
+    }
+    .roox-alert > .title {
+        font-weight: bold;
+    }
+</style>
+<script>
+    const roox = {};
+    roox.alert = function(html, title = '<?php echo TEXT_INFO; ?>', classBg = 'success', duration = 5000){
+        const element = $("<div class='alert alert-"+classBg+" roox-alert' role='alert'><div class='title'>"+title+"</div>"+html+"</div>").appendTo($('.header.navbar'));
+        setTimeout(function(){
+            const fadeTime = 500;
+            const slideTime = 500;
+            let opacity = 1;
+            const fadeInterval = setInterval(function () {
+                if (opacity > 0) {
+                    opacity -= 0.1;
+                    element.css('opacity', opacity);
+                } else {
+                    clearInterval(fadeInterval);
+                    let height = element[0].offsetHeight;
+                    const slideInterval = setInterval(function () {
+                        if (height > 0) {
+                            height -= 10;
+                            element.css('height', height);
+                        } else {
+                            clearInterval(slideInterval);
+                            element.remove();
+                        }
+                    }, slideTime / 10);
+                }
+            }, fadeTime / 10);
+        }, duration);
+    }
+</script>
 <?php 
-require("plugins/".ROOX_PLUGIN."/setting.php");
-foreach (explode(',', ROOX_MODULES) as $module) 
+global ${ROOX_PLUGIN . "_modules"};
+foreach (${ROOX_PLUGIN . "_modules"} as $module) 
 {
-    $path = "plugins/".ROOX_PLUGIN."/modules/$module/head.php";
+    $path = component_path(ROOX_PLUGIN . "/{$module}/head");  
     if(is_file($path))
     {
-        require($path);
+        require $path;
     }
 }
 ?>
