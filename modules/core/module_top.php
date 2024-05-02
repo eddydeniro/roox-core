69bd3c40c4f1e9d5e4fec11cac41900175e9da4b
Edi Supriyadi <eddydeniro@users.noreply.github.com>
2024-05-02 20:36:56 +0700

    Revise version

--- modules/core/module_top.php
+++ modules/core/module_top.php
@@ -2,6 +2,6 @@
   $app_title = app_set_title(ucwords(ROOX_PLUGIN) . " &raquo; " . TEXT_SETTINGS);
   $plugin_name = ROOX_PLUGIN;
   $module_name = 'core';
-  $module_version = '1.1';
+  $module_version = '1.2';
   $module_url = 'https://github.com/eddydeniro/roox-core';
 ?>
