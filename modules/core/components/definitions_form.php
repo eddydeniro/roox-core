aa879c520d70fbf9bc0b8847171c0e68d7b1a1a3
Edi Supriyadi <eddydeniro@users.noreply.github.com>
2024-05-02 09:37:12 +0700

    Add HTML editor for long and/or complex definitions in Dictionary

--- modules/core/components/definitions_form.php
+++ modules/core/components/definitions_form.php
@@ -23,15 +23,51 @@
             ?>
                 <tr>
                     <td><div style="padding:8px 0;"><?php echo $values['dict_key'] ?></div></td>
-                    <td><?php echo input_tag("definitions[{$id}]", $values['dict_value'], array('class' => 'form-control transparent', 'style'=>'width:100%')) ?></td>
+                    <td><i class="fa fa-edit showDialog" onclick="showDialog('<?php echo "definitions_{$id}"; ?>')" style="position:absolute;margin-top:10px;cursor:pointer;"></i><?php echo input_tag("definitions[{$id}]", $values['dict_value'], array('class' => 'form-control transparent', 'style'=>'margin-left:20px;width:100%')) ?></td>
                 </tr>  
             <?php endforeach; ?>
         </tbody>
     </table>
 </div>
 </form>
+
+<dialog id="favDialog" class="dialog">
+  <div class="modal-header">  			
+	<h4 class="modal-title"><?php echo TEXT_EDIT; ?></h4>
+  </div>
+  <form method="dialog" id="dialogForm">
+  <div class="modal-body">
+	<div class="form-body">
+        <textarea name="def_value" id="def_value"></textarea>
+	</div>
+  </div>
+  <div class="modal-footer">
+  	<button class="btn btn-warning" value="cancel" onclick="closeDialog()"><?php echo TEXT_CANCEL; ?></button>
+    <button class="btn btn-primary" value="default" onclick="saveDialog()"><?php echo TEXT_SAVE; ?></button>
+  </div>
+  </form>
+</dialog>
+
 <script>
     $(document).ready(function () {
         $('#definition').DataTable();
     });
+    function showDialog(id){
+        $('#def_value').val($('#'+id).val());
+        $('#def_value').data('target', id);
+        CKEDITOR.replace('def_value', {
+            startupFocus: true
+        });    
+        setTimeout(function(){
+            $('#favDialog')[0].showModal();
+        }, 500);
+    }
+    function closeDialog(){
+        CKEDITOR.instances['def_value'].destroy(true);        
+    }
+    function saveDialog(){
+        const target = $('#def_value').data('target');
+        $('#'+target).val(CKEDITOR.instances['def_value'].getData());
+        CKEDITOR.instances['def_value'].destroy(true);        
+    }
 </script>
